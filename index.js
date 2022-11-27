d7e7b257819128a16f2aa74c96973da0c1a3a6b9
Salahuddin Ahmed <salahuddinahmed8544@gmail.com>
2022-11-26 20:57:18 -0800

    bookngs id loaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000 ;
 
 
@@ -73,7 +73,13 @@ async function run(){
             })
 
 
-
+            // bookings id data loaded
+            app.get('/bookings/:id', async (req,res)=>{
+                const id = req.params.id;
+                const query = {_id:ObjectId(id)};
+                const booking = await buyerBookingsCollection.findOne(query)
+                res.send(booking)
+            })
     }
     finally{
 
